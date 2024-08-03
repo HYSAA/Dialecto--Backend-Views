3b4a1c36f7d5264e60ae4b713202911b4f2d524d
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2024-08-03 15:11:21 +0800

    try commit
    duplicate

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -18,7 +18,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Create New Course</h2>
+                <h2>Create New Courseasdasdasda</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('courses.index') }}"> Back</a>
