6a88c970d4055106e6202dfdecbf959be9186e9d
Archilece <johnrigelqgaleos@gmail.com>
2024-08-12 21:19:13 +0800

    same file from before

--- resources/views/contents/show.blade.php
+++ resources/views/contents/show.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="main-container">
+    <br><br>
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
                 <h2>{{ $course->name }} - {{ $lesson->title }} - Content</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('courses.lessons.contents.index', [$course->id, $lesson->id]) }}"> Back</a>
+                <a class="btn btn-primary" href="{{  route('courses.lessons.show', [$course->id,$lesson->id]) }}"> Back To Lesson</a>
+                <!-- Fixed IT  -->
+                <a class="btn btn-primary" type="button" href="{{  route('courses.lessons.contents.show', [$course->id, $lesson->id, $content->id - 1]) }}">Back</a>
+
             </div>
         </div>
     </div>
@@ -20,10 +24,30 @@
                 {{ $content->text }}
                 @if ($content->image)
                                     <img src="{{ asset('storage/' . $content->image) }}" width="150px">
-                                @endif
-
+                @endif
             </div>
         </div>
+    </div>
+    <!-- Dialect -->
+    <div class="row justify-content-center">
+        
+            <b class="fs-1" style="font-size: 50px">{{ $content->text }}</b>
+        
+    </div>
+    <!-- English Equivalent -->
+    <div class="row justify-content-center">
+        
+            <i class="fs-1" style="font-size: 40px">{{ $content->text }} This would be in english</i>
+        
+    </div>
+    <div class="row justify-content-center">
+        <h2>Template for videos</h2>
+    </div>
+
+
+<!-- KANI RA AJ AKAOH GE USOB GE PLUS 1 ang ID pero ka sunod ani is sa controller para maka back sa before na content ug forward  -->
+    <div class="row justify-content-center">
+        <a class="btn btn-primary" type="button" href="{{  route('courses.lessons.contents.show', [$course->id, $lesson->id, $content->id + 1]) }}">proceed</a>
     </div>
 </div>
 @endsection
