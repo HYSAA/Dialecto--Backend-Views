9c70587ba253733501e07a163184abac1d67af00
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2024-08-20 10:20:58 +0800

    added a condition that the user can only get request
    condition for only get request

--- routes/api.php
+++ routes/api.php
@@ -25,14 +25,19 @@ use App\Http\Controllers\Api\HomeController as ApiHomeController;
 
     //API ROUTES FOR THE USER TO GET REQUEST
 
-    Route::prefix('user')->group(function () {
-        // Route::get('dashboard', [ApiHomeController::class, 'index']);
-        Route::resource('courses',ApiCourseController::class);
-        Route::resource('courses.lessons', ApiLessonController::class);     
-        Route::resource('courses.lessons.contents', ApiContentController::class);
-        Route::resource('courses.lessons.contents.questions', ApiQuestionController::class);
-        Route::resource('courses.lessons.contents.questions.answers', ApiAnswerController::class);  
-    });
+   // User Routes (GET requests only)
+Route::prefix('user')->group(function () {
+    Route::get('courses', [ApiCourseController::class, 'index']);
+    Route::get('courses/{course}', [ApiCourseController::class, 'show']);
+    Route::get('courses/{course}/lessons', [ApiLessonController::class, 'index']);
+    Route::get('courses/{course}/lessons/{lesson}', [ApiLessonController::class, 'show']);
+    Route::get('courses/{course}/lessons/{lesson}/contents', [ApiContentController::class, 'index']);
+    Route::get('courses/{course}/lessons/{lesson}/contents/{content}', [ApiContentController::class, 'show']);
+    Route::get('courses/{course}/lessons/{lesson}/contents/{content}/questions', [ApiQuestionController::class, 'index']);
+    Route::get('courses/{course}/lessons/{lesson}/contents/{content}/questions/{question}', [ApiQuestionController::class, 'show']);
+    Route::get('courses/{course}/lessons/{lesson}/contents/{content}/questions/{question}/answers', [ApiAnswerController::class, 'index']);
+    Route::get('courses/{course}/lessons/{lesson}/contents/{content}/questions/{question}/answers/{answer}', [ApiAnswerController::class, 'show']);
+});
 
 
 
