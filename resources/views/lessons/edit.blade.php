e4fe0c28e92a8ab4a363ad8ac59897513309fcc7
Archilece <johnrigelqgaleos@gmail.com>
2024-08-31 15:01:23 +0800

    lesson edit fixed image

--- resources/views/lessons/edit.blade.php
+++ resources/views/lessons/edit.blade.php
@@ -32,7 +32,7 @@
     </div>
     @endif -->
 
-    <form action="{{ route('admin.lessons.update', [$course->id, $lesson->id]) }}" method="POST">
+    <form action="{{ route('admin.lessons.update', [$course->id, $lesson->id]) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
 
@@ -40,20 +40,24 @@
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Title:</strong>
-                    <input type="text" name="title" value="{{ $lesson->title }}" class="form-control" placeholder="Title">
+                    <input type="text" name="title" value="{{ $lesson->title }}" class="form-control" placeholder="Title" required>
                 </div>
             </div>
 
+            
 
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <label for="image"><strong>Lesson Image:</strong> </label>
+                    <input type="file" class="form-control" name="image" id="image">
+                    <small>If you want to change the image, upload a new one. Otherwise, leave it blank.</small>
+                </div>
+            </div>
 
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                 <button type="submit" class="btn btn-primary">Save</button>
                 <a class="btn btn-danger" href="{{ route('admin.courses.index') }}">Discard</a>
             </div>
-
-
-
-
         </div>
     </form>
 </div>
