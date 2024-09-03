90aa2a216c8fc4071ba029234421ed25437a4eff
Archilece <johnrigelqgaleos@gmail.com>
2024-09-03 17:31:01 +0800

    Added the Name of The User to View Progression

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,7 @@
 
     <div class="row">
         <div class="col-lg-12 ">
-            <h2> Current Progression : {Name of User}  </h2>
+            <h2> Current Progression of {{$user->name}}  </h2>
         </div>
     </div>
 
