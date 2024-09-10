f7c93220e0f19b49fb4a148750694dfafb6cb205
Archilece <johnrigelqgaleos@gmail.com>
2024-09-10 23:08:30 +0800

    Added Edit Function in the Suggestion

--- routes/web.php
+++ routes/web.php
@@ -235,6 +235,7 @@ Route::middleware(['auth', 'user'])->prefix('user')->group(function () {
 
 
     Route::get('/wordSuggested/{id}/viewUpdateSelected', [UserController::class, 'viewUpdateSelected'])->name('user.viewUpdateSelected');
+    Route::post('/wordSuggested/{id}/updateSelected', [UserController::class, 'updateSelected'])->name('user.updateSelected');
 
     Route::get('/wordSuggested', [UserController::class, 'wordSuggested'])->name('user.wordSuggested');
     Route::get('/selectUserCourseLesson', [UserController::class, 'selectUserCourseLesson'])->name('user.selectUserCourseLesson');
