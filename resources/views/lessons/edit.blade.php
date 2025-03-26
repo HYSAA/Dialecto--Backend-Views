169c2aa0d0886156d5c7c13052cb34980f4eb1dc
Galeos-John <johnrigelqgaleos@gmail.com>
2025-03-26 10:59:24 +0800

    added error handling for admin lesson update

--- resources/views/lessons/edit.blade.php
+++ resources/views/lessons/edit.blade.php
@@ -10,6 +10,17 @@
         </div>
     </div>
 
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
     <form action="{{ route('admin.lessons.update', [$courseId, $lessonId]) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
